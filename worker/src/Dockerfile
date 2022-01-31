e842d1ea93774cdae7cecbb8e98d6254f9ff2412
Thomas.Chaigneau <thomas.chaigneau@arkea.com>
2022-01-31 16:30:36 +0100

    init worker files

---
+++ worker/src/Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.9.5-slim-buster
+
+WORKDIR /usr/src/app
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN pip install --upgrade pip
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY . .
---
+++ worker/src/worker.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+from celery import Celery
+
+
+celery = Celery(__name__)
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
+celery.conf.result_backend = os.enviro.get("")
+
+
+@celery.task(name="launch_training")
+def launch_training():
+    pass
