8c5bad82cd9e8528d09b95db31ebb159ea643122
Thomas.Chaigneau <thomas.chaigneau@arkea.com>
2022-01-31 14:46:56 +0100

    init interface

---
+++ interface/app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+
+st.set_page_config(
+    layout="centered", page_title="Make Us Rich", page_icon=":moneybag:"
+)
+st.title("Make Us Rich")
+st.subheader("Cryptocurrency Forecasting Dashboard")
+st.markdown("""
+    ![Version](https://img.shields.io/badge/Version-1.0-green)
+    ![Streamlit](https://img.shields.io/badge/Streamlit-1.4.0-yellow)
+    ![License](https://img.shields.io/badge/License-Apache_2.0-orange)
+""")
