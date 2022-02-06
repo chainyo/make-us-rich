7bcdd42b3d5e606668310e8056ee854384c0438a
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2022-02-06 13:56:42 +0100

    add update_models endpoint

--- src/serving/app.py
+++ src/serving/app.py
@@ -5,6 +5,8 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
+from model_loader import ModelLoader
+
 
 app = FastAPI(
     title="Serving Crypto Models",
@@ -13,6 +15,8 @@ app = FastAPI(
     openapi_url="/api/v1/openapi.json",
 )
 
+model_loader = ModelLoader()
+
 
 @app.get("/", include_in_schema=False)
 async def home():
@@ -20,6 +24,33 @@ async def home():
     Home endpoint to redirect to docs.
     """
     return RedirectResponse("/docs")
+
+
+@app.get("/predict", include_in_schema=True)
+async def predict(currency: str, compare: str):
+    """
+    Predict endpoint.
+
+    Parameters
+    ----------
+    currency: str
+        Currency used in the model.
+    compare: str
+        Compare used in the model.
+    
+    Returns
+    -------
+    """
+    return None
+
+
+@app.put("/update_models", include_in_schema=True)
+async def update_model():
+    """
+    Update models endpoint.
+    """
+    model_loader.update_model_files()
+    return {"message": "All models have been updated."}
 
 
 if __name__ == "__main__":
