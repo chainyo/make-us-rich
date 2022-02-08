9ea19eee83ccd73376bb8f04d1b94043f173a04d
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2022-02-08 10:36:49 +0100

    add random_string function

---
+++ src/interface/app/utils.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+
+CHARACTERS = string.ascii_letters + string.digits + string.punctuation
+
+
+def random_string(length: int = 25) -> str:
+    """
+    Generate a random string of the specified length.
+
+    Parameters:
+    -----------
+    length: int
+        The length of the string. Default is 25.
+    
+    Returns:
+    --------
+    str:
+        The random generated string.
+    """
+    return ''.join(random.choice(CHARACTERS) for _ in range(length))
