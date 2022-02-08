5bea701eb228777e9381928479f9ee25227907ee
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2022-02-08 16:53:29 +0100

    add role

--- src/interface/app/app.py
+++ src/interface/app/app.py
@@ -8,12 +8,19 @@ st.set_page_config(
     layout="centered", page_title="Make Us Rich", page_icon=":moneybag:"
 )
 st.title("Make Us Rich")
-st.subheader("Cryptocurrency Forecasting Dashboard")
+st.subheader("Cryptocurrency Forecasting Dashboard 📈")
 st.markdown("""
     ![Version](https://img.shields.io/badge/Version-1.0-green)
     ![Streamlit](https://img.shields.io/badge/Streamlit-1.4.0-yellow)
     ![License](https://img.shields.io/badge/License-Apache_2.0-orange)
 """)
+with st.expander("About"):
+    st.markdown("""
+    """)
 
 authentication = Authentication()
-username, authentication_status = authentication.login("login")
+username, role, authentication_status = authentication.login("login")
+
+if authentication_status:
+    pass
+
