04b6be2c3cca446860c41acc12a956dca2a017c0
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2022-02-10 14:49:41 +0100

    package prefect worker as a class

---
+++ src/worker/prefect_flow.py
@@ -0,0 +1,166 @@
+from pathlib import Path
+from typing import Dict
+
+from prefect import Client, Flow
+from prefect.utilities.exceptions import ClientError
+
+from kedro.framework.project import pipelines
+from kedro.framework.session import KedroSession
+from kedro.framework.startup import bootstrap_project, ProjectMetadata
+from kedro.io import MemoryDataSet
+
+from kedro_task import KedroTask
+
+
+class WorkerPrefect:
+
+    TARGETED_COINS = ["btc", "eth", "chz"]
+    
+    def __init__(self, path: Path = None) -> None:
+        """
+        Initialize a Prefect Flow from a Kedro project.
+
+        Parameters
+        ----------
+        project_path: Path, optional
+            Path to the Kedro project. If not provided, the current working directory is used.
+        
+        Returns
+        -------
+        None
+        """
+        self.metadata = self._get_kedro_project_metadata(path)
+        self.client = Client()
+        self.registered_flows = self._build_flows()
+
+    
+    def __repr__(self) -> str:
+        return f"WorkerPrefectFlow(project_path={self.metadata.project_path})"
+
+    
+    def list_registered_flows(self) -> Dict[str, Flow]:
+        """
+        List registered flows.
+
+        Returns
+        -------
+        Dict
+            Registered flows.
+        """
+        return self.registered_flows
+        
+
+    def _build_flows(self) -> Dict[str, Flow]:
+        """
+        Create one flow for each currency and add it to available flows.
+
+        Returns
+        -------
+        Dict
+            Available flows.
+        """
+        registered_flows = {}
+        for currency in self.TARGETED_COINS:
+            registered_flows[currency] = self._build_one_flow(currency)
+        return registered_flows
+
+
+    def _build_one_flow(self, currency: str, compare: str = "usdt", register: bool = True) -> Flow:
+        """
+        Build a Prefect Flow from a Kedro project.
+
+        Parameters
+        ----------
+        metadata: ProjectMetadata
+            Metadata of the Kedro project.
+        currency: str
+            Currency used in the model.
+        compare: str
+            Compare used in the model. Default is "usdt".
+        
+        Returns
+        -------
+        Flow
+            Prefect Flow built from the Kedro project.
+        """
+        session = KedroSession.create(
+            project_path=self.metadata.project_path, extra_params={"currency": currency, "compare": compare}
+        )
+        context = session.load_context()
+        catalog = context.catalog
+        pipeline_name = "__default__"
+        pipeline = pipelines.get(pipeline_name)
+        unregistered_ds = pipeline.data_sets() - set(catalog.list())
+
+        for ds_name in unregistered_ds:
+            catalog.add(ds_name, MemoryDataSet())
+
+        with Flow(f"{currency}_{compare}") as flow:
+            tasks = {}
+            for node, parent_nodes in pipeline.node_dependencies.items():
+                if node._unique_key not in tasks:
+                    node_task = KedroTask(node, catalog)
+                    tasks[node._unique_key] = node_task
+                else:
+                    node_task = tasks[node._unique_key]
+                
+                parent_tasks = []
+                for parent in parent_nodes:
+                    if parent._unique_key not in tasks:
+                        parent_task = KedroTask(parent, catalog)
+                        tasks[parent._unique_key] = parent_task
+                    else:
+                        parent_task = tasks[parent._unique_key]
+                    parent_tasks.append(parent_task)
+
+                flow.set_dependencies(task=node_task, upstream_tasks=parent_tasks)
+
+        if register:
+            flow.register(self.metadata.project_name)
+
+        return flow
+
+
+    def _get_kedro_project_metadata(self, path: Path) -> ProjectMetadata:
+        """
+        Get the metadata of a Kedro project.
+
+        Parameters
+        ----------
+        project_path: Path, optional
+            Path to the Kedro project. If not provided, the current working directory is used.
+        
+        Returns
+        -------
+        dict
+            Metadata of the Kedro project.
+        """
+        if path is None:
+            parent_dir = Path(__file__).resolve().parents[1]
+            project_path = parent_dir.joinpath("trainer")
+        else:
+            project_path = Path(path)
+        metadata = bootstrap_project(project_path)
+        return metadata
+
+
+    def _initialize_project(self) -> None:
+        """
+        Initialize a Kedro project inside a Prefect Flow project.
+
+        Parameters
+        ----------
+        project_name: str
+            Name of the Prefect Flow project.
+        """
+        try:
+            self.client.create_project(project_name=self.metadata.project_name)
+        except ClientError:
+            # `project_name` project already exists
+            pass
+
+
+
+if __name__ == "__main__":
+    flow = WorkerPrefect()
+    print(flow.list_registered_flows())
