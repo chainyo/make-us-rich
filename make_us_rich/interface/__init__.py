3799fea6f17ebb604f666969152117b748c1eab2
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2022-02-11 15:24:09 +0100

    add class/fct to init

---
+++ make_us_rich/interface/__init__.py
@@ -0,0 +1,8 @@
+from .api_request import ApiRequest
+from .authentication import Authentication
+from .database_handler import DatabaseHandler
+from .plots import (
+    candlestick_plot,
+    format_data,
+    scatter_plot,
+)
