721a21fc1f16dc59d2948487bd331aa64e17e662
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2022-02-20 13:49:18 +0100

    add fct to get env variables for runs

--- make_us_rich/cli/utils.py
+++ make_us_rich/cli/utils.py
@@ -1,7 +1,8 @@
 import typer
 
-from typing import List
+from typing import List, Dict
 
+from make_us_rich.utils import load_env
 from . import (
     ALL, 
     COMPONENTS,
@@ -68,3 +69,31 @@ def ask_user_about_environment() -> bool:
     question = "Are you in the right environment?\nWe are going to install some required dependencies."
     typer.confirm(question, abort=True)
     return True
+
+
+def env_variables(files: List[str]) -> Dict[str, Dict]:
+    """
+    Get the environment variables from the listed files. It will raise an error if the file does not exist. It will
+    also raise an error if env variables are not valid, e.g. if they are equal to `changeme`.
+
+    Parameters
+    ----------
+    files: List[str]
+        List of files to check.
+    
+    Returns
+    -------
+    Dict[str, Dict]
+        Dictionary of environment variables.
+    """
+    config = {}
+    for file in files:
+        variables = load_env(file)
+
+        for key, value in variables.items():
+            if value == "changeme":
+                raise typer.BadParameter(f"You need to set the {key} environment variable.")
+
+        config[file] = variables
+
+    return config
