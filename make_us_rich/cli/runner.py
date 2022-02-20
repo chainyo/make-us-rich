9a0728042d24257d3d67afa8f1b0aebe712818b2
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2022-02-20 13:41:09 +0100

    add func for running interface

--- make_us_rich/cli/runner.py
+++ make_us_rich/cli/runner.py
@@ -2,7 +2,7 @@ import docker
 import typer
 
 from . import COMPONENTS
-from .utils import ask_user_about_environment
+from .utils import ask_user_about_environment, env_variables
 
 
 class ComponentRunner:
@@ -72,10 +72,48 @@ class ComponentRunner:
         return True
 
     
-    def _run_interface(self):
-        """"""
-        print("Running interface")
+    def _run_interface(self) -> bool:
+        """
+        Run the interface.
 
+        Returns
+        -------
+        bool
+            True if the service exists, raises an error otherwise.
+        """
+        typer.echo("Pulling images needed for the interface\n")
+        self.client.images.pull("postgres", tag="13.4")
+        self.client.images.pull("dpage/pgadmin4", tag="snapshot")
+        
+        typer.echo("Checking env variables\n")
+        config = env_variables(["pgadmin", "postgres", "api"])
+
+        typer.echo("Building postgres database\n")
+        self.client.containers.run(
+            "postgres", name="postgres", restart_policy={"Name": "unless-stopped"},
+            environment=config["postgres"], ports={5432: 5432}, detach=True,
+            volumes={
+                "./database/init.sql": {"bind": "/docker-entrypoint-initdb.d/init.sql", "mode": "rw"},
+                "./database/postgres-data": {"bind": "/var/lib/postgresql/data", "mode": "rw"},
+            },
+        )
+        typer.echo("Building pgadmin\n")
+
+        self.client.containers.run(
+            "dpage/pgadmin4", name="pgadmin", restart_policy={"Name": "unless-stopped"},
+            environment=config["pgadmin"], ports={5050: 80}, detach=True,
+            volumes={"pgadmin-data": {"bind": "/var/lib/pgadmin", "mode": "rw"}},
+        )
+        typer.echo("Building interface\n")
+
+        self.client.images.build(fileobj="Dockerfile", tag="mkrich-interface:latest")
+        config_for_interface = config["postgres"] + config["api"]
+        self.client.containers.run(
+            "mkrich-interface:latest", name="interface", restart_policy={"Name": "unless-stopped"},
+            environment=config_for_interface, ports={8501: 8501}, detach=True,
+        )
+        
+        return True
 
     def _run_serving(self):
         """"""
